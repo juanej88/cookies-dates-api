0290c411b724b8531dbb164c1f0e57d929a2d9bf
Juan Espinosa <arq.jorrin@gmail.com>
2024-10-15 13:35:40 +0800

    Set throttle rates

--- cookiesdates/settings.py
+++ cookiesdates/settings.py
@@ -83,6 +83,14 @@ REST_FRAMEWORK = {
 		'rest_framework.authentication.TokenAuthentication',
 		'rest_framework.authentication.SessionAuthentication',
   ],
+  'DEFAULT_THROTTLE_CLASSES': [
+			'rest_framework.throttling.AnonRateThrottle',
+			'rest_framework.throttling.UserRateThrottle',
+	],
+	'DEFAULT_THROTTLE_RATES': {
+			'anon': '15/minute',
+			'user': '30/minute',
+	},
 }
 
 
